3f33e7ddcb40509d0152880932a0465bde60dafb
MxYz1101 <m.yousufzai@student.han.nl>
2024-10-23 18:31:35 +0200

    header wijzigingen

--- applicatie/html/Header.php
+++ applicatie/html/Header.php
@@ -3,11 +3,10 @@ $header = '<header>
         <div class="container">
             <h1>Pizzeria Sole Machina</h1>
             <ul>
-                <li><a href="menu.html">Menu</a></li>
-                <li><a href="winkelmandje.html">Winkelmandje</a></li>
-                <li><a href="profiel.html">Profiel</a></li>
-                <li><a href="login.html">Login</a></li>
-                <li><a href="registratie.html">Registratie</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="winkelmandje.php">Winkelmandje</a></li>
+                <li><a href="profiel.php">Profiel</a></li>
+                <li><a href="registratie.php">Login of registratie</a></li>
             </ul>
         </div>
     </header>';
